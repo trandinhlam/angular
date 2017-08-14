8487a55cf3e0df1a1ec405e90142f853ec06b04a
ALLEXCEED\lamtd <lamtd@allexceed.vn>
2017-08-14 18:05:08 +0700

    tạo app todolist

---
+++ todo-app/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import {Todo} from './todo';
+import {TodoDataService} from './todo-data.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  
+  providers: [
+  	TodoDataService
+  ]
+})
+export class AppComponent {
+	newTodo : Todo = new Todo();	
+	title = 'app';
+	// todos: Todo[] = [];
+	constructor(
+		private todoDataService: TodoDataService
+	)
+	{
+		this.todoDataService = todoDataService;
+	}
+
+	addTodo()
+	{
+		this.todoDataService.addTodo(this.newTodo);
+	}
+
+	toggleTodoComplete(todo)
+	{
+		this.todoDataService.toggleTodoComplete(todo);
+	}
+
+	removeTodo(todo)
+	{
+		this.todoDataService.deleteTodoById(todo.id);
+	}
+
+
+	get todos() 
+	{
+   		return this.todoDataService.getAllTodos();
+  	}
+}
