1025598191fc9482d787cf77976370d949471bcc
ALLEXCEED\lamtd <lamtd@allexceed.vn>
2017-08-15 10:43:26 +0700

    App todo list: Căn bản hoàn thành. có copy style của Tác giả

--- todo-app/src/app/app.component.spec.ts
+++ todo-app/src/app/app.component.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodoDataService } from './todo-data.service';
+import { Todo } from './todo';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule
+      ],
       declarations: [
         AppComponent
+      ],
+      providers: [
+        TodoDataService,
       ],
     }).compileComponents();
   }));
@@ -27,6 +36,12 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('app');
+  }));
+
+  it('should have a newTodo todo', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app.newTodo instanceof Todo).toBeTruthy();
   }));
 });
--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -5,7 +5,7 @@ import {TodoDataService} from './todo-data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  // styleUrls: ['./app.component.css'],
   
   providers: [
   	TodoDataService
@@ -19,12 +19,13 @@ export class AppComponent {
 		private todoDataService: TodoDataService
 	)
 	{
-		this.todoDataService = todoDataService;
+		// this.todoDataService = todoDataService;
 	}
 
 	addTodo()
 	{
 		this.todoDataService.addTodo(this.newTodo);
+		this.newTodo = new Todo();
 	}
 
 	toggleTodoComplete(todo)
